4ecdef2403b1bc5c4986159464e03d0027241c1a
SalmaAshraf20 <salma.ashraf.mahmoud2000@gmail.com>
2024-05-19 15:33:10 +0300

    first one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './commonComponent/footer/footer.component';
+import { HeaderComponent } from './commonComponent/header/header.component';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    PortfolioComponent,
+    ContactComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    NotfoundComponent,
   ],
   imports: [
     BrowserModule,
